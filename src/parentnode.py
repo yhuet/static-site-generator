52b42667e46f976855b9c30da35480660db6844c
yhuet <yann@huet.dev>
2025-07-18 00:15:43 +0200

    ParentNode

---
+++ src/parentnode.py
@@ -0,0 +1,16 @@
+from htmlnode import HTMLNode
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("ParentNode doesn't have a tag.")
+        if not self.children:
+            raise ValueError("ParentNode doesn't have any children.")
+        html = f"<{self.tag}{self.props_to_html()}>"
+        for child in self.children:
+            html += child.to_html()
+        html += f"</{self.tag}>"
+        return html
---
+++ src/test_parentnode.py
@@ -0,0 +1,40 @@
+import unittest
+
+from leafnode import LeafNode
+from parentnode import ParentNode
+
+class TestParentNode(unittest.TestCase):
+
+    def test_example(self):
+        node = ParentNode(
+            "p",
+            [
+                LeafNode("b", "Bold text"),
+                LeafNode(None, "Normal text"),
+                LeafNode("i", "italic text"),
+                LeafNode(None, "Normal text"),
+            ],
+        )
+        self.assertEqual(node.to_html(), "<p><b>Bold text</b>Normal text<i>italic text</i>Normal text</p>")
+
+
+    def test_to_html_with_children(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span><b>grandchild</b></span></div>",
+        )
+
+    def test_to_html_with_no_children(self):
+        node = ParentNode(
+            "p",
+            [],
+        )
+        self.assertRaises(ValueError, node.to_html)
