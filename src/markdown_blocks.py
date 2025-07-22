c7340147510a5954139247243451cdedee6d2e18
yhuet <yann@huet.dev>
2025-07-22 21:00:17 +0200

    Block to HTML

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,4 +1,8 @@
 from enum import Enum
+from parentnode import ParentNode
+from textnode import TextNode, TextType
+from inline_markdown import text_to_textnodes
+from text_to_html import text_node_to_html_node
 
 class BlockType(Enum):
     PARAGRAPH = "paragraph"
@@ -42,3 +46,113 @@ def block_to_block_type(block: str):
             i += 1
         return BlockType.ORDERED_LIST
     return BlockType.PARAGRAPH
+
+def text_to_children(text):
+    children = []
+    text_nodes = text_to_textnodes(text)
+    for text_node in text_nodes:
+        html_node = text_node_to_html_node(text_node)
+        children.append(html_node)
+    return children
+
+def lines_to_list_item(lines):
+    items = []
+    for line in lines:
+        children = text_to_children(line)
+        node = ParentNode("li", children)
+        items.append(node)
+    return items
+
+def block_paragraph(block):
+    tag = "p"
+    lines = block.split("\n")
+    paragraph = " ".join(lines)
+    children = text_to_children(paragraph)
+    return ParentNode(tag, children)
+
+def block_heading(block):
+    if block.startswith("# "):
+        tag = "h1"
+        text = block[2:]
+    elif block.startswith("## "):
+        tag = "h2"
+        text = block[3:]
+    elif block.startswith("### "):
+        tag = "h3"
+        text = block[4:]
+    elif block.startswith("#### "):
+        tag = "h4"
+        text = block[5:]
+    elif block.startswith("##### "):
+        tag = "h5"
+        text = block[6:]
+    elif block.startswith("###### "):
+        tag = "h6"
+        text = block[7:]
+    children = text_to_children(text)
+    return ParentNode(tag, children)
+
+def block_code(block):
+    text = block[4:-3]
+    text_node = TextNode(text, TextType.TEXT)
+    html_node = text_node_to_html_node(text_node)
+    code = ParentNode("code", [html_node])
+    return ParentNode("pre", [code])
+
+def block_quote(block):
+    tag = "blockquote"
+    lines = block.split("\n")
+    new_lines = []
+    for line in lines:
+        new_lines.append(line[2:])
+    text = "\n".join(new_lines)
+    children = text_to_children(text)
+    return ParentNode(tag, children)
+
+def block_unordered_list(block):
+    tag = "ul"
+    lines = block.split("\n")
+    new_lines = []
+    for line in lines:
+        new_lines.append(line[2:])
+    children = lines_to_list_item(new_lines)
+    return ParentNode(tag, children)
+
+def block_ordered_list(block):
+    tag = "ol"
+    lines = block.split("\n")
+    new_lines = []
+    i = 1
+    j = 3
+    for line in lines:
+        if i > 9:
+            j = 4 
+        new_lines.append(line[j:])
+        i += 1
+    children = lines_to_list_item(new_lines)
+    return ParentNode(tag, children)
+
+def block_to_html_node(block):
+    block_type = block_to_block_type(block)
+    if block_type == BlockType.PARAGRAPH:
+        return block_paragraph(block)
+    if block_type == BlockType.HEADING:
+        return block_heading(block)
+    if block_type == BlockType.CODE:
+        return block_code(block)
+    if block_type == BlockType.QUOTE:
+        return block_quote(block)
+    if block_type == BlockType.UNORDERED_LIST:
+        return block_unordered_list(block)
+    if block_type == BlockType.ORDERED_LIST:
+        return block_ordered_list(block)
+
+
+
+def markdown_to_html_node(markdown):
+    blocks = markdown_to_blocks(markdown)
+    children = []
+    for block in blocks:
+        node = block_to_html_node(block)
+        children.append(node)
+    return ParentNode("div", children)
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,6 +1,6 @@
 import unittest
 
-from markdown_blocks import markdown_to_blocks, block_to_block_type, BlockType
+from markdown_blocks import markdown_to_blocks, block_to_block_type, BlockType, markdown_to_html_node
 
 class TestBlockMarkdown(unittest.TestCase):
     def test_markdown_to_blocks(self):
@@ -81,3 +81,51 @@ Paragraph continued
         block = "A simple paragraph"
         block_type = block_to_block_type(block)
         self.assertEqual(block_type, BlockType.PARAGRAPH)
+
+    def test_paragraphs(self):
+        md = """
+This is **bolded** paragraph text in a p tag here
+
+This is another paragraph with _italic_ text and `code` here
+
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",
+        )
+
+    def test_codeblock(self):
+        md = """
+```
+This is text that _should_ remain
+the **same** even with inline stuff
+```
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><pre><code>This is text that _should_ remain\nthe **same** even with inline stuff\n</code></pre></div>",
+        )
+
+    def test_lists(self):
+        md = """
+- some
+- **items**
+
+1. one
+2. two
+3. three
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><ul><li>some</li><li><b>items</b></li></ul><ol><li>one</li><li>two</li><li>three</li></ol></div>"
+        )
+        
