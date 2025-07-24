fdb05ae8dfdb2abcfcdc0eecf7a88990a24d10d4
yhuet <yann@huet.dev>
2025-07-24 17:25:04 +0200

    Deploy to the Internet

--- src/generate.py
+++ src/generate.py
@@ -9,7 +9,7 @@ def extract_title(markdown):
             return line.lstrip("#").strip()
     raise Exception("missing title")
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path) as f:
         md = f.read()
@@ -19,12 +19,14 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(md)
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", content)
+    template = template.replace('href="/', f'href="{basepath}')
+    template = template.replace('src="/', f'src="{basepath}')
     if not os.path.exists(os.path.dirname(dest_path)):
         os.makedirs(os.path.dirname(dest_path))
     with open(dest_path, "w") as d:
         d.write(template)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for filename in os.listdir(dir_path_content):
         from_path = os.path.join(dir_path_content, filename)
         dest_path = os.path.join(dest_dir_path, filename)
@@ -32,12 +34,14 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
             generate_pages_recursive(
                 from_path,
                 template_path,
-                dest_path
+                dest_path,
+                basepath
                 )
         else:
             dest_path = Path(dest_path).with_suffix(".html")
             generate_page(
                 from_path,
                 template_path,
-                dest_path
+                dest_path,
+                basepath
             )
--- src/main.py
+++ src/main.py
@@ -1,12 +1,15 @@
 import os
 import shutil
+import sys
 
 from copystatic import copy_files_recursive
 from generate import generate_pages_recursive
 
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_public = "./docs"
+dir_path_content = "./content"
+template_path = "./template.html"
 
 
 def main():
@@ -17,7 +20,12 @@ def main():
     print("Copying static files to public directory...")
     copy_files_recursive(dir_path_static, dir_path_public)
 
-    generate_pages_recursive("content", "template.html", "public")
+    if len (sys.argv) > 1:
+        basepath = sys.argv[1]
+    else:
+        basepath = "/"
+
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public, basepath)
 
 if __name__ == "__main__":
     main()
